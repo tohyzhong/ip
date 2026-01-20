fa66e41a6a7efe5b0496e5705b02e1fd8b255db0
Yi Zhong <tohyzhong@gmail.com>
2026-01-21 00:34:05 +0800

    feat: Add ToDo tasks

--- src/main/java/Gigachad.java
+++ src/main/java/Gigachad.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Gigachad {
     public enum Command {
-        BYE, LIST, ADD, MARK, UNMARK;
+        BYE, LIST, ADD, MARK, UNMARK, TODO;
 
         public static Command getCommand(String userInput) {
             try {
@@ -21,7 +21,7 @@ public class Gigachad {
         String name = "Gigachad";
         System.out.println(String.format("%s\n\tHello! I'm %s.\n\tWhat can I do for you?\n%s",
                 horizontalString, name, horizontalString));
-        
+
         // Echo Loop
         String userInput = "";
         while (true) {
@@ -62,12 +62,19 @@ public class Gigachad {
                             tasks.get(Integer.parseInt(userInput.split(" ")[1]) - 1).setDone(),
                             horizontalString));
                     break;
-                
+
                 case UNMARK:
                     System.out.println(String.format("%s\n\t%s\n%s",
                             horizontalString,
                             tasks.get(Integer.parseInt(userInput.split(" ")[1]) - 1).setNotDone(),
                             horizontalString));
+                    break;
+
+                case TODO:
+                    userInput = userInput.substring(5);
+                    tasks.add(new ToDo(userInput));
+                    System.out.println(String.format("%s\n\tadded: %s\n%s",
+                            horizontalString, userInput, horizontalString));
                     break;
             }
         }

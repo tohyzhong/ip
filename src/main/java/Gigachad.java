8711f73dccce943947c44f8a62b87c8179ab868d
Yi Zhong <tohyzhong@gmail.com>
2026-01-20 23:04:15 +0800

    feat: Add echo and exit from command line inputs

--- src/main/java/Gigachad.java
+++ src/main/java/Gigachad.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Gigachad {
     public static void main(String[] args) {
-        String horizontalString = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        String horizontalString = "\t____________________________________________________________";
         String name = "Gigachad";
-        System.out.println(String.format("%s\nHello! I'm %s.\nWhat can I do for you?\n%s",
+        System.out.println(String.format("%s\n\tHello! I'm %s.\n\tWhat can I do for you?\n%s",
                 horizontalString, name, horizontalString));
-        System.out.println(String.format("Bye. Hope to see you again soon!\n%s", horizontalString));
+        
+        // Echo Loop
+        String userInput = "";
+        while (true) {
+            System.out.print("\nEnter a command (bye to exit): ");
+            userInput = scanner.nextLine();
+            if (userInput.toLowerCase().equals("bye")) {
+                break;
+            }
+
+            System.out.println(String.format("%s\n\t%s\n%s", horizontalString, userInput, horizontalString));
+        }
+        
+        System.out.println(String.format("%s\n\tBye. Hope to see you again soon!\n%s", horizontalString, horizontalString));
+        scanner.close();
     }
 }

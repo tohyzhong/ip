c1ac58d1971d3a245a4a7980790f536a07a25caa
Yi Zhong <tohyzhong@gmail.com>
2026-01-20 22:45:09 +0800

    Rename Duke to Gigachad and greet

---
+++ src/main/java/Gigachad.java
@@ -0,0 +1,9 @@
+public class Gigachad {
+    public static void main(String[] args) {
+        String horizontalString = "____________________________________________________________";
+        String name = "Gigachad";
+        System.out.println(String.format("%s\nHello! I'm %s.\nWhat can I do for you?\n%s",
+                horizontalString, name, horizontalString));
+        System.out.println(String.format("Bye. Hope to see you again soon!\n%s", horizontalString));
+    }
+}

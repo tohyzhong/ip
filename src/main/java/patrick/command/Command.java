4d870706a78dd13e2d865f99a25a7a906fe5ac2f
Yi Zhong <tohyzhong@gmail.com>
2026-01-27 23:51:15 +0800

    docs: Add JavaDoc for Command

--- src/main/java/patrick/command/Command.java
+++ src/main/java/patrick/command/Command.java
@@ -5,6 +5,10 @@ import patrick.storage.Storage;
 import patrick.task.TaskList;
 import patrick.ui.Ui;
 
+/**
+ * Represents the collection of executable commands supported by Patrick.
+ * Each enum corresponds to a valid command, or an error.
+ */
 public enum Command {
     BYE,
     LIST,
@@ -18,6 +22,15 @@ public enum Command {
     DUE,
     ON;
 
+    /**
+     * Parses user input string and returns corresponding Command.
+     * First term of input string is extracted and matched against pre-defined
+     * Command enums (case-insensitive).
+     * 
+     * @param userInput The raw user input string.
+     * @return The matching {@code Command} constant, or {@code ERROR} for invalid
+     *         commands.
+     */
     public static Command getCommand(String userInput) {
         try {
             return Command.valueOf(userInput.split(" ")[0].toUpperCase());
@@ -26,6 +39,19 @@ public enum Command {
         }
     }
 
+    /**
+     * Executes the logic for the corresponding command.
+     * Dispatcher method to call relevant Command method based on the enum constant.
+     * 
+     * @param tasks     The list containing the current tasks.
+     * @param ui        The user interface object for displaying output.
+     * @param userInput The raw user input string.
+     * @param storage   The storage object used for loading and saving changes
+     *                  locally.
+     * @throws PatrickException If an error occurs during execution of any command
+     *                          (e.g. file read and write errors, invalid command
+     *                          parameters)
+     */
     public void execute(TaskList tasks, Ui ui, String userInput, Storage storage) throws PatrickException {
         switch (this) {
         case BYE:

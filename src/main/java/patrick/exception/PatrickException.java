97519e796718b72f57c0816b3bc8c8daef5e0128
Yi Zhong <tohyzhong@gmail.com>
2026-01-28 00:34:30 +0800

    docs: Add JavaDoc for custom exceptions

--- src/main/java/patrick/exception/PatrickException.java
+++ src/main/java/patrick/exception/PatrickException.java
@@ -1,5 +1,10 @@
 package patrick.exception;
 
+/**
+ * The base exception class for Patrick.
+ * All custom exceptions thrown by Patrick's functionality will extend
+ * {@code PatrickException}.
+ */
 public class PatrickException extends Exception {
     public PatrickException(String message) {
         super(message);
